de29f5208104e1f278e07663d59996b1213b1eb0
julioMarvim <juliomarvim@yahoo.com.br>
2023-05-16 14:41:40 -0300

    feat: criando modelo de usuario

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes.js';
+//Importando nossa database.
+import '../src/database/index.js';
 
 dotenv.config();
 
